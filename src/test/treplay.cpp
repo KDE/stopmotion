4f1355c1201567bbef15a1abde6489549f94843a
Tim Band <t.b@n.w>
2013-07-16 13:56:02 +0100

    Some test adaptation to the new CommandFactory

--- src/test/treplay.cpp
+++ src/test/treplay.cpp
@@ -53,15 +53,11 @@ public:
 	public:
 		std::string name;
 		std::string s1;
-		std::string s2;
 		int32_t i1;
 		int32_t i2;
-		int32_t i3;
-		int32_t i4;
-		int32_t i5;
 		EtCommand(std::string commandName)
-				: name(commandName), s1(""), s2(""),
-				  i1(no_num), i2(no_num), i3(no_num), i4(no_num), i5(no_num) {
+				: name(commandName), s1(""),
+				  i1(no_num), i2(no_num) {
 		}
 		Command& DoAtomic() {
 			onExecute();
@@ -70,12 +66,8 @@ public:
 		bool operator==(const EtCommand& other) const {
 			return name == other.name
 					&& s1 == other.s1
-					&& s2 == other.s2
 					&& i1 == other.i1
-					&& i2 == other.i2
-					&& i3 == other.i3
-					&& i4 == other.i4
-					&& i5 == other.i5;
+					&& i2 == other.i2;
 		}
 		bool operator!=(const EtCommand& other) const {
 			return !(*this == other);
@@ -83,70 +75,12 @@ public:
 	};
 	~EmptyTestCommandFactory() {
 	}
-	Command& Make() const {
+	Command* Create(Parameters& ps) {
 		EtCommand* e = new EtCommand(name);
-		return *e;
-	}
-	Command& Make(int32_t a) const {
-		EtCommand* e = new EtCommand(name);
-		e->i1 = a;
-		return *e;
-	}
-	Command& Make(int32_t a, int32_t b) const {
-		EtCommand* e = new EtCommand(name);
-		e->i1 = a;
-		e->i2 = b;
-		return *e;
-	}
-	Command& Make(int32_t a, int32_t b, int32_t c) const {
-		EtCommand* e = new EtCommand(name);
-		e->i1 = a;
-		e->i2 = b;
-		e->i3 = c;
-		return *e;
-	}
-	Command& Make(int32_t a, int32_t b, int32_t c, int32_t d) const {
-		EtCommand* e = new EtCommand(name);
-		e->i1 = a;
-		e->i2 = b;
-		e->i3 = c;
-		e->i4 = d;
-		return *e;
-	}
-	Command& Make(int32_t a, int32_t b, int32_t c, int32_t d, int32_t f) const {
-		EtCommand* e = new EtCommand(name);
-		e->i1 = a;
-		e->i2 = b;
-		e->i3 = c;
-		e->i4 = d;
-		e->i5 = f;
-		return *e;
-	}
-	Command& Make(const char* s) const {
-		EtCommand* e = new EtCommand(name);
-		e->s1 = s;
-		return *e;
-	}
-	Command& Make(int32_t a, const char* s) const {
-		EtCommand* e = new EtCommand(name);
-		e->i1 = a;
-		e->s1 = s;
-		return *e;
-	}
-	Command& Make(int32_t a, int32_t b, const char* s) const {
-		EtCommand* e = new EtCommand(name);
-		e->i1 = a;
-		e->i2 = b;
-		e->s1 = s;
-		return *e;
-	}
-	Command& Make(int32_t a, int32_t b, int32_t c, const char* s) const {
-		EtCommand* e = new EtCommand(name);
-		e->i1 = a;
-		e->i2 = b;
-		e->i3 = c;
-		e->s1 = s;
-		return *e;
+		e->i1 = ps.GetInteger();
+		e->s1 = ps.GetString();
+		e->i2 = ps.GetInteger();
+		return e;
 	}
 };
 
@@ -223,39 +157,17 @@ public:
 };
 
 void TestEtCommand(Command* c,
-		int32_t exi1, int32_t exi2, int32_t exi3, int32_t exi4, int32_t exi5,
-		const char* exs) {
+		int32_t exi1, int32_t exi2, const char* exs) {
 	EmptyTestCommandFactory::EtCommand* e
 			= static_cast<EmptyTestCommandFactory::EtCommand*>(c);
 	QCOMPARE(e->i1, exi1);
 	QCOMPARE(e->i2, exi2);
-	QCOMPARE(e->i3, exi3);
-	QCOMPARE(e->i4, exi4);
-	QCOMPARE(e->i5, exi5);
 	QCOMPARE(e->s1.c_str(), exs);
 }
 
 void TestCommandFactory::allMakeCallsParse() {
-	std::auto_ptr<Command> c(&cr->MakeCommand("et"));
-	TestEtCommand(c.get(), no_num, no_num, no_num, no_num, no_num, "");
-	std::auto_ptr<Command> ci(&cr->MakeCommand("et 5"));
-	TestEtCommand(ci.get(), 5, no_num, no_num, no_num, no_num, "");
-	std::auto_ptr<Command> cii(&cr->MakeCommand("et -5 412345"));
-	TestEtCommand(cii.get(), -5, 412345, no_num, no_num, no_num, "");
-	std::auto_ptr<Command> ciii(&cr->MakeCommand("et 765 5 3"));
-	TestEtCommand(ciii.get(), 765, 5, 3, no_num, no_num, "");
-	std::auto_ptr<Command> ciiii(&cr->MakeCommand("et 1 2 3 4"));
-	TestEtCommand(ciiii.get(), 1, 2, 3, 4, no_num, "");
-	std::auto_ptr<Command> ciiiii(&cr->MakeCommand("et 1 2 3 4 5"));
-	TestEtCommand(ciiiii.get(), 1, 2, 3, 4, 5, "");
-	std::auto_ptr<Command> cs(&cr->MakeCommand("et \"jim\""));
-	TestEtCommand(cs.get(), no_num, no_num, no_num, no_num, no_num, "jim");
-	std::auto_ptr<Command> cis(&cr->MakeCommand("et 0 \"ziggy\""));
-	TestEtCommand(cis.get(), 0, no_num, no_num, no_num, no_num, "ziggy");
-	std::auto_ptr<Command> ciis(&cr->MakeCommand("et -56 -925235 \"ziggy\""));
-	TestEtCommand(ciis.get(), -56, -925235, no_num, no_num, no_num, "ziggy");
-	std::auto_ptr<Command> ciiis(&cr->MakeCommand("et 1 2 10000000 \"ziggy\""));
-	TestEtCommand(ciiis.get(), 1, 2, 10000000, no_num, no_num, "ziggy");
+	std::auto_ptr<Command> cii(&cr->MakeCommand("et -5 \"hello world!\" 412345"));
+	TestEtCommand(cii.get(), -5, 412345, "hello world!");
 }
 
 int32_t randomInt() {
@@ -272,75 +184,12 @@ void TestCommandFactory::parsingDescriptionIsCloning() {
 		const char* commandName = rand() % 2 == 0? "et" : "sec";
 		const CommandFactory* cf = cr->GetCommandFactory(commandName);
 		std::auto_ptr<Command> c;
-		int32_t i1, i2, i3, i4, i5;
-		std::string s1;
-		switch(rand() % 10) {
-		case 0:
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make()");
-			c.reset(&cf->Make());
-			break;
-		case 1:
-			i1 = randomInt();
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make(%d)", i1);
-			c.reset(&cf->Make(i1));
-			break;
-		case 2:
-			i1 = randomInt();
-			i2 = randomInt();
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make(%d, %d)", i1, i2);
-			c.reset(&cf->Make(i1, i2));
-			break;
-		case 3:
-			i1 = randomInt();
-			i2 = randomInt();
-			i3 = randomInt();
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make(%d, %d, %d)", i1, i2, i3);
-			c.reset(&cf->Make(i1, i2, i3));
-			break;
-		case 4:
-			i1 = randomInt();
-			i2 = randomInt();
-			i3 = randomInt();
-			i4 = randomInt();
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make(%d, %d, %d, %d)", i1, i2, i3, i4);
-			c.reset(&cf->Make(i1, i2, i3, i4));
-			break;
-		case 5:
-			i1 = randomInt();
-			i2 = randomInt();
-			i3 = randomInt();
-			i4 = randomInt();
-			i5 = randomInt();
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make(%d, %d, %d, %d, %d)", i1, i2, i3, i4, i5);
-			c.reset(&cf->Make(i1, i2, i3, i4, i5));
-			break;
-		case 6:
-			s1 = RandomString();
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make(%s)", s1.c_str());
-			c.reset(&cf->Make(s1.c_str()));
-			break;
-		case 7:
-			i1 = randomInt();
-			s1 = RandomString();
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make(%d, %s)", i1, s1.c_str());
-			c.reset(&cf->Make(i1, s1.c_str()));
-			break;
-		case 8:
-			i1 = randomInt();
-			i2 = randomInt();
-			s1 = RandomString();
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make(%d, %d, %s)", i1, i2, s1.c_str());
-			c.reset(&cf->Make(i1, i2, s1.c_str()));
-			break;
-		case 9:
-			i1 = randomInt();
-			i2 = randomInt();
-			i3 = randomInt();
-			s1 = RandomString();
-			snprintf(description, sizeof(description)/sizeof(description[0]), "Make(%d, %d, %d, %s)", i1, i2, i3, s1.c_str());
-			c.reset(&cf->Make(i1, i2, i3, s1.c_str()));
-			break;
-		}
+		int32_t i1 = randomInt();
+		int32_t i2 = randomInt();
+		std::string s1 = RandomString();
+		snprintf(description, sizeof(description)/sizeof(description[0]),
+				"Make(%d, %s, %d)", i1, s1.c_str(), i2);
+		c.reset(cf->Create(i1, s1.c_str(), i2));
 		QVERIFY2(*static_cast<const EmptyTestCommandFactory::EtCommand*>(c.get())
 				== *static_cast<const EmptyTestCommandFactory::EtCommand*>(cl.GetClone()),
 				description);
@@ -382,8 +231,10 @@ public:
 	};
 	AddCharFactory(std::string* m) : model(m) {
 	}
-	Command& Make(int32_t character, int32_t position) const {
-		return *new AddChar(*model, character, position);
+	Command* Create(Parameters& ps) {
+		int32_t character = ps.GetInteger();
+		int32_t position = ps.GetInteger();
+		return new AddChar(*model, character, position);
 	}
 };
 
@@ -401,8 +252,9 @@ public:
 	};
 	DelCharFactory(std::string* m) : model(m) {
 	}
-	Command& Make(int32_t position) const {
-		return *new DelChar(*model, position);
+	Command* Create(Parameters& ps) {
+		int32_t position = ps.GetInteger();
+		return new DelChar(*model, position);
 	}
 };
 
