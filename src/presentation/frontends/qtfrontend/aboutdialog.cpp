2536c424400a9a93feedb77ebc5d9ae1ebefaf20
Tim Band <t.b@n.w>
2013-11-30 22:34:35 +0000

    Added acknowledgements

--- src/presentation/frontends/qtfrontend/aboutdialog.cpp
+++ src/presentation/frontends/qtfrontend/aboutdialog.cpp
@@ -51,6 +51,10 @@ AboutDialog::AboutDialog(QWidget *parent)
 				"Eric Stolten &lt;stoltene2@gmail.com&gt;<br>"
 				"Josh Green &lt;josh@resonance.org&gt;<br>"
 				"Jehan Pagès<br>"
+				"Tim Band<br>"
+				"Ying-Chun Liu (PaulLiu) &lt;paulliu@debian.org&gt;<br>"
+				"Barak A. Pearlmutter &lt;barak+git@cs.nuim.ie&gt;<br>"
+				"Cyril Brulebois &lt;kibi@debian.org&gt;<br>"
                                 "</p>"),
 				tr("A&uthors"));
 	
@@ -67,6 +71,7 @@ AboutDialog::AboutDialog(QWidget *parent)
 				"Koray Löker &lt;loker@pardus.org.tr&gt; (" + tr("Turkish") + ")<br>"
 				"Diego Giordano &lt;fusion_machine@tin.it&gt; (" + tr("Italian") + ")<br>"
 				"Matteo Comisso &lt;teomatteo8.9@hotmail.it&gt; (" + tr("Italian") + ")<br>"
+				"Yaron (sh-yaron) (Hebrew)<br>"
 				"</p>"
 				"<p><b>" + tr("Logo") + "</b><br>"
 				"Gorazd Bizjak &lt;gorazd@zapstudio.net&gt;<br>"
